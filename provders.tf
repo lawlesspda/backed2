d6cf750e2b0783ea69516c01a95df169c0d2a0aa
paul lawless <lawlesspaul3737@gmail.com>
2024-12-11 13:58:27 +0000

    1234

--- provders.tf
+++ provders.tf
@@ -3,3 +3,45 @@ provider "aws" {
   profile = "default"
 
 }
+
+resource "aws_instance" "example" {
+  ami             = "ami-0453ec754f44f9a4a"
+  instance_type   = "t2.micro"
+  security_groups = aws_security_group.allow_8080
+
+  user_data = <<-EOF
+            #!/bin/bash
+            sudo yum update -y
+            sudo yum install httpd -y
+            sudo systemctl start httpd
+            sudo systemctl enable httpd
+            echo "<h1>Welcome to the Web Server - Deployed via Terraform</h1>" > /var/www/html/index.html
+            EOF
+
+  tags = {
+    Name = "terraform example"
+  }
+
+}
+
+resource "aws_security_group" "allow_8080" {
+  name        = "allow-8080"
+  description = "Allow inbound traffic on port 8080"
+
+  ingress {
+    description = "Allow HTTP traffic on port 8080"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allows traffic from all IPs
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # All protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
